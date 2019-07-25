b20102f09368b0764b4929c374d91a247a6f1f82
kaizame <kais.fa@students.amikom.ac.id>
2019-07-25 15:07:34 +0700

    tugas uas

---
+++ Produk.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAS
+{
+    public class Produk
+    {
+
+        public string kode { get; set; }
+        public string nama { get; set; }
+        public int beli { get; set; }
+        public int jual { get; set; }
+
+    }
+}
